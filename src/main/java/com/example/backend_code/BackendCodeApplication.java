cbfd430bc747ed6a6c1a540ed1699b2a9ffa604d
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-04-21 16:01:19 +0100

    first commit

---
+++ src/main/java/com/example/backend_code/BackendCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCodeApplication.class, args);
+	}
+
+}
