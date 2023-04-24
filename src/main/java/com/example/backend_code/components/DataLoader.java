63051aeb0ed7315b083c98fdbd1b6759a470490b
maryan axmed <12maxmed@gmail.com>
2023-04-24 16:12:30 +0100

    populating dataloader

--- src/main/java/com/example/backend_code/components/DataLoader.java
+++ src/main/java/com/example/backend_code/components/DataLoader.java
@@ -1,4 +1,37 @@
 package com.example.backend_code.components;
 
-public class DataLoader {
+import com.example.backend_code.models.User;
+import com.example.backend_code.repositories.ItemRepository;
+import com.example.backend_code.repositories.ToDoListRepository;
+import com.example.backend_code.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    ToDoListRepository toDoListRepository;
+
+    public DataLoader(){
+
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+//        make user
+        User user = new User("John", 1L);
+        userRepository.save(user);
+
+
+
+    }
 }
