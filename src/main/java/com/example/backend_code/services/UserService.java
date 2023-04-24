e17e58c45bb5616ccd8c7f9e85b6ba3115dd1198
Hayan Butt <hayan_880@hotmail.com>
2023-04-24 13:11:30 +0100

    added user classes

--- src/main/java/com/example/backend_code/services/UserService.java
+++ src/main/java/com/example/backend_code/services/UserService.java
@@ -1,4 +1,20 @@
 package com.example.backend_code.services;
 
+import com.example.backend_code.models.User;
+import com.example.backend_code.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findByUser(String name) {
+        return userRepository.findByName(name);
+    }
+
+
+
 }
