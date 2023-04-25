c6e2485e6abc317a195e10b560541f2620630dbc
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-04-25 10:02:16 +0100

    updated user Service and Controller

--- src/main/java/com/example/backend_code/controllers/UserController.java
+++ src/main/java/com/example/backend_code/controllers/UserController.java
@@ -18,33 +18,37 @@ public class UserController {
     @Autowired
     UserService userService;
 
-
+    // INDEX
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
     }
 
+    // SHOW
     @GetMapping (value = "/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return new ResponseEntity<>(userService.findUserById(id), HttpStatus.OK);
     }
 
+    // CREATE
     @PostMapping
     public ResponseEntity<User> postUser(@RequestBody User user) {
         userService.createUser(user);
         return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
+    // DELETE
     @DeleteMapping (value = "/{id}")
     public ResponseEntity<Long> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
-
-
-
-
-
+    // UPDATE
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable Long id){
+        userService.updateUser(user, id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/example/backend_code/services/UserService.java
+++ src/main/java/com/example/backend_code/services/UserService.java
@@ -1,5 +1,6 @@
 package com.example.backend_code.services;
 
+import com.example.backend_code.models.ToDoList;
 import com.example.backend_code.models.User;
 import com.example.backend_code.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,14 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User findUserById(Long id){
+        return userRepository.findById(id).get();
+    }
+
     public User findByUserByName(String name) {
         return userRepository.findByName(name);
     }
@@ -25,12 +34,17 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
-    public User findUserById(Long id){
-        return userRepository.findById(id).get();
+    public void removeUserFromToDoList(Long id){
+        User foundUser = userRepository.getById(id);
+        for (ToDoList toDoList : foundUser.getMasterList()) {
+            toDoList.removeUser(foundUser);
+        }
     }
 
-    public List<User> findAllUsers(){
-        return userRepository.findAll();
+    public void updateUser(User user, Long id){
+        User userToUpdate = userRepository.findById(id).get();
+        userToUpdate.setName(user.getName());
+        userToUpdate.setMasterList(user.getMasterList());
+        userRepository.save(userToUpdate);
     }
-
 }
