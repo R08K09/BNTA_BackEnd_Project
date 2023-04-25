5838292a0d534e3cdbd62cb59d30ddda89db6e6b
Vincent Adeola <vince.adeola@outlook.com>
2023-04-25 09:59:46 +0100

    adds post and delete mapping to listcntrl

--- src/main/java/com/example/backend_code/controllers/ListController.java
+++ src/main/java/com/example/backend_code/controllers/ListController.java
@@ -1,15 +1,10 @@
 package com.example.backend_code.controllers;
-
-
 import com.example.backend_code.models.ToDoList;
 import com.example.backend_code.services.ToDoListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,5 +24,17 @@ public class ListController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ToDoList> getListById(@PathVariable Long id){
         return new ResponseEntity<>(toDoListService.findListById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ToDoList> createList (@RequestBody ToDoList toDoList){
+        toDoListService.createList(toDoList);
+        return new ResponseEntity<>(toDoList, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteList(@PathVariable Long id){
+        toDoListService.deleteList(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
